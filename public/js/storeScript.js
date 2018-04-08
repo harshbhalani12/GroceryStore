243450e01d5c538916e8e73539586cab0435816a
bhalaniharsh <bhalanihp@gmail.com>
2018-04-08 10:53:00 -0500

    fixed angular error due to ngFileUpload

--- public/js/storeScript.js
+++ public/js/storeScript.js
@@ -1,4 +1,4 @@
-var app = angular.module('groceryStore', ['ngRoute','ngResource','ngFileUpload']);
+var app = angular.module('groceryStore', ['ngRoute','ngResource']);
 
 app.config(function($routeProvider){
 	$routeProvider
