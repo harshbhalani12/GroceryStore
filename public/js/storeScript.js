f5bbc1d375201dfed479b40a22b3396a911dd9af
Utkarsh <utkarshgandhi96@gmail.com>
2018-04-13 16:52:20 -0500

    Update Prodcuts Controller

--- public/js/storeScript.js
+++ public/js/storeScript.js
@@ -1,282 +1,330 @@
-var app = angular.module('groceryStore', ['ngRoute','ngResource']);
+var app = angular.module('groceryStore', ['ngRoute', 'ngResource']);
 
-app.config(function($routeProvider){
-	$routeProvider
-		//the timeline display
-		.when('/',{
-			templateUrl: 'products.html',
-			controller: 'productCtrl'
-		})
-		.when('/login', {
-			templateUrl: 'login.html',
-			controller: 'authController'
-		})
-		.when('/register',{
-			templateUrl: 'register.html',
-			controller: 'authController'
-		})
-		.when('/manage_products',{
-			templateUrl: 'manage_products.html',
-			controller: 'manageProductsCtrl'
-		});
+app.config(function($routeProvider) {
+    $routeProvider
+    //the timeline display
+        .when('/', {
+            templateUrl: 'products.html',
+            controller: 'productCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'login.html',
+            controller: 'authController'
+        })
+        .when('/register', {
+            templateUrl: 'register.html',
+            controller: 'authController'
+        })
+        .when('/update_products', {
+            templateUrl: 'update_products.html',
+            controller: 'updateProductsCtrl'
+        })
+        .when('/manage_products', {
+            templateUrl: 'manage_products.html',
+            controller: 'manageProductsCtrl'
+        });
 });
 
-app.controller('headerController',function($scope,$http,$resource,$location,authService){
-	var User = $resource('/api/user');
-	User.get({},function(user){
-		if(user.name){
-			$scope.authenticated = true;
-			authService.setAuthenticated(true);
-			if(user.admin){
-				$scope.admin = true;
-				authService.setIsAdmin(true);
-			}
-		}
-	});
-	$scope.$watch(authService.getAuthenticated, function(auth){
-		$scope.authenticated = auth;
-	});
+app.controller('headerController', function($scope, $http, $resource, $location, authService) {
+    var User = $resource('/api/user');
+    User.get({}, function(user) {
+        if (user.name) {
+            $scope.authenticated = true;
+            authService.setAuthenticated(true);
+            if (user.admin) {
+                $scope.admin = true;
+                authService.setIsAdmin(true);
+            }
+        }
+    });
+    $scope.$watch(authService.getAuthenticated, function(auth) {
+        $scope.authenticated = auth;
+    });
 
-	$scope.$watch(authService.getIsAdmin,function(admin){
-		$scope.admin = admin;
-	});
-	
-	$scope.signout = function(){
-			$http.post('/auth/signout').then(function success(response){
-				authService.reset();
-				$scope.authenticated = false;
-				$scope.admin = false;
-				$location.path('/login');
-		});
-	};
+    $scope.$watch(authService.getIsAdmin, function(admin) {
+        $scope.admin = admin;
+    });
+
+    $scope.signout = function() {
+        $http.post('/auth/signout').then(function success(response) {
+            authService.reset();
+            $scope.authenticated = false;
+            $scope.admin = false;
+            $location.path('/login');
+        });
+    };
 });
 
-app.controller('authController', function($scope, $http, $location,msgService,authService){
-	$scope.user = {email: '', password: ''};
-	$scope.error_message = '';
-	$scope.strength_message = '';
-	$scope.$watch(msgService.getSuccessMessage,function(message){
-		$scope.success_message=message;
-	});
-	$scope.$watch(msgService.getErrorMessage, function(message){
-		$scope.error_message = message;
-	});
-    $scope.login = function(){
-    	$http.post('/auth/login', $scope.user).then(function success(response){
-				if(response.data.state == true){
-					authService.setAuthenticated(true);
-					authService.setIsAdmin(response.data.isAdmin);
-					msgService.reset();
-					$location.path('/');
-				}
-				else{
-					$scope.error_message = response.data.message;
-				}
-			}, function error(error){
-				$scope.error_message = error;
-		});
-  	};
+app.controller('authController', function($scope, $http, $location, msgService, authService) {
+    $scope.user = { email: '', password: '' };
+    $scope.error_message = '';
+    $scope.strength_message = '';
+    $scope.$watch(msgService.getSuccessMessage, function(message) {
+        $scope.success_message = message;
+    });
+    $scope.$watch(msgService.getErrorMessage, function(message) {
+        $scope.error_message = message;
+    });
+    $scope.login = function() {
+        $http.post('/auth/login', $scope.user).then(function success(response) {
+            if (response.data.state == true) {
+                authService.setAuthenticated(true);
+                authService.setIsAdmin(response.data.isAdmin);
+                msgService.reset();
+                $location.path('/');
+            } else {
+                $scope.error_message = response.data.message;
+            }
+        }, function error(error) {
+            $scope.error_message = error;
+        });
+    };
 
-    $scope.register = function(){
-		if($scope.isValidForm()){
-			$http.post('/auth/signup', $scope.user).then(function success(response){
-				if(response.data.state == true){
-					msgService.setSuccessMessage(response.data.message);
-					$location.path('/login');
-				}
-				else{
-					$scope.error_message = response.data.message;
-				}
-			}
-			,function error(error){
-				$scope.error_message = error;
-			});
-		}
-	};
-	
-	$scope.isValidForm = function(){
-		return $scope.isValidEmail() && $scope.checkPwdStrength();
-	};
+    $scope.register = function() {
+        if ($scope.isValidForm()) {
+            $http.post('/auth/signup', $scope.user).then(function success(response) {
+                if (response.data.state == true) {
+                    msgService.setSuccessMessage(response.data.message);
+                    $location.path('/login');
+                } else {
+                    $scope.error_message = response.data.message;
+                }
+            }, function error(error) {
+                $scope.error_message = error;
+            });
+        }
+    };
 
-	$scope.isValidEmail = function(){
-		var email = $scope.user.email;
-		if(!email.match(/^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9_\-\.]+\.[a-z]{3}/)){
-			$scope.email_invalid = "Email format: abc@pqr.xyz. Only alphanumeric characters,'-' and '_' are allowed, and only lowercase alphabetical characters are allowed in domain name.";
-			return false;
-		}
-		return true;
-	}
+    $scope.isValidForm = function() {
+        return $scope.isValidEmail() && $scope.checkPwdStrength();
+    };
 
-	$scope.checkPwdStrength = function(){
-		$scope.strength_message = '';
-		var pwd = $scope.user.password;
-		if(pwd){
-			var strength = 0;
-			if(pwd.length >= 10 && pwd.length <= 15){
-				strength ++;
-			}
-			else{
-				$scope.strength_message = "Password length should be between 10 and 15 characters.";
-				return false;
-			}
-			if(pwd.match(/[a-z]+/)){
-				strength ++;
-			}
-			if(pwd.match(/[A-Z]+/)){
-				strength ++;
-			}
-			if(pwd.match(/\d+/)){
-				strength ++; 
-			}
-			if(pwd.match(/([!@#$%\^&*+=._-]+)/)){
-				strength ++;
-			}
-			if(strength == 1){
-				$scope.strength_message = "Bad password. Please enter a stronger password. (Password must include "
-					+"at least 1 uppercase, 1 lowercase letter, 1 digit and 1 special character)";
-			}
-			else if(strength == 2){
-				$scope.strength_message = "Medium password. Please enter a stronger password. (Password must include "
-				+"at least 1 uppercase, 1 lowercase letter, 1 digit and 1 special character)";
-			}
-			else if(strength == 3){
-				$scope.strength_message = "Good password, but please enter a stronger password. (Password must include "
-				+"at least 1 uppercase, 1 lowercase letter, 1 digit and 1 special character)";
-			}
-			else if(strength == 4){
-				$scope.strength_message = "Strong password, but please enter a stronger password. (Password must include "
-				+"at least 1 uppercase, 1 lowercase letter, 1 digit and 1 special character)";
-				return false;
-			}
-			else{
-				$scope.strength_message = "Excellent password! All conditions satisfied!";
-				return true;
-			}
-			return false;
-		}
-	}
+    $scope.isValidEmail = function() {
+        var email = $scope.user.email;
+        if (!email.match(/^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9_\-\.]+\.[a-z]{3}/)) {
+            $scope.email_invalid = "Email format: abc@pqr.xyz. Only alphanumeric characters,'-' and '_' are allowed, and only lowercase alphabetical characters are allowed in domain name.";
+            return false;
+        }
+        return true;
+    }
+
+    $scope.checkPwdStrength = function() {
+        $scope.strength_message = '';
+        var pwd = $scope.user.password;
+        if (pwd) {
+            var strength = 0;
+            if (pwd.length >= 10 && pwd.length <= 15) {
+                strength++;
+            } else {
+                $scope.strength_message = "Password length should be between 10 and 15 characters.";
+                return false;
+            }
+            if (pwd.match(/[a-z]+/)) {
+                strength++;
+            }
+            if (pwd.match(/[A-Z]+/)) {
+                strength++;
+            }
+            if (pwd.match(/\d+/)) {
+                strength++;
+            }
+            if (pwd.match(/([!@#$%\^&*+=._-]+)/)) {
+                strength++;
+            }
+            if (strength == 1) {
+                $scope.strength_message = "Bad password. Please enter a stronger password. (Password must include " +
+                    "at least 1 uppercase, 1 lowercase letter, 1 digit and 1 special character)";
+            } else if (strength == 2) {
+                $scope.strength_message = "Medium password. Please enter a stronger password. (Password must include " +
+                    "at least 1 uppercase, 1 lowercase letter, 1 digit and 1 special character)";
+            } else if (strength == 3) {
+                $scope.strength_message = "Good password, but please enter a stronger password. (Password must include " +
+                    "at least 1 uppercase, 1 lowercase letter, 1 digit and 1 special character)";
+            } else if (strength == 4) {
+                $scope.strength_message = "Strong password, but please enter a stronger password. (Password must include " +
+                    "at least 1 uppercase, 1 lowercase letter, 1 digit and 1 special character)";
+                return false;
+            } else {
+                $scope.strength_message = "Excellent password! All conditions satisfied!";
+                return true;
+            }
+            return false;
+        }
+    }
 });
 
-app.controller('manageProductsCtrl',function($scope,$resource,$location,authService){
-	
-	var User = $resource('/api/user');
-	User.get({},function(user){
-		if(user.name){
-			$scope.authenticated = true;
-			authService.setAuthenticated(true);
-			if(user.admin){
-				$scope.admin = true;
-				authService.setIsAdmin(true);
-				$scope.data = {
-					operations:[
-						{id:'1', name : 'Add'	},
-						{id:'2', name : 'Update'},
-						{id:'3', name : 'Delete'}
-					],
-					categories:[
-						{id: '1', name: 'Category1'},
-						{id: '2', name: 'Category2'},
-						{id: '3', name: 'Category3'}
-					]
-				};
-			}
-			else{
-				$location.path('/login');
-			}
-		}
-		else{
-			$location.path('/login');
-		}
-	});
+app.controller('manageProductsCtrl', function($scope, $resource, $location, authService) {
 
-	$scope.$watch(authService.getAuthenticated, function(auth){
-		$scope.authenticated = auth;
-	});
+    var User = $resource('/api/user');
+    User.get({}, function(user) {
+        if (user.name) {
+            $scope.authenticated = true;
+            authService.setAuthenticated(true);
+            if (user.admin) {
+                $scope.admin = true;
+                authService.setIsAdmin(true);
+                $scope.data = {
+                    operations: [
+                        { id: '1', name: 'Add' },
+                        { id: '2', name: 'Update' },
+                        { id: '3', name: 'Delete' }
+                    ],
+                    categories: [
+                        { id: '1', name: 'Category1' },
+                        { id: '2', name: 'Category2' },
+                        { id: '3', name: 'Category3' }
+                    ]
+                };
+            } else {
+                $location.path('/login');
+            }
+        } else {
+            $location.path('/login');
+        }
+    });
 
-	$scope.$watch(authService.getIsAdmin,function(admin){
-		$scope.admin = admin;
-	});
+    $scope.$watch(authService.getAuthenticated, function(auth) {
+        $scope.authenticated = auth;
+    });
 
-	
+    $scope.$watch(authService.getIsAdmin, function(admin) {
+        $scope.admin = admin;
+    });
+
+
 
 });
 
-app.controller('productCtrl',function($scope,$resource,$location,authService){
-	
-	$scope.$watch(authService.getAuthenticated, function(auth){
-		$scope.authenticated = auth;
-	});
+app.controller('updateProductsCtrl', function($scope, $rootScope, $resource, $location, authService) {
+    // auto-filling details of the product
+    $scope.productName = $rootScope.prod.productName;
+    $scope.price = $rootScope.prod.productPrice;
+    $scope.quantity = $rootScope.prod.stockQuantity;
 
-	$scope.$watch(authService.getIsAdmin,function(admin){
-		$scope.admin = admin;
-	});
-	var User = $resource('/api/user');
-	User.get({},function(user){
-		if(user.name){
-			$scope.authenticated = true;
-			authService.setAuthenticated(true);
-			if(user.admin){
-				$scope.admin = true;
-				authService.setIsAdmin(true);
-			}
-			var Products = $resource('/api/products');
-			Products.query({},function(products,err){
-				$scope.products = products;
-			});
-		}
-		else{
-			$location.path('/login');
-		}
-	});
+    var User = $resource('/api/user');
+    User.get({}, function(user) {
+        if (user.name) {
+            $scope.authenticated = true;
+            authService.setAuthenticated(true);
+            if (user.admin) {
+                $scope.admin = true;
+                authService.setIsAdmin(true);
+                $scope.data = {
+                    categories: [
+                        { id: '1', name: 'Category1' },
+                        { id: '2', name: 'Category2' },
+                        { id: '3', name: 'Category3' }
+                    ]
+                };
+            } else {
+                $location.path('/login');
+            }
+        } else {
+            $location.path('/login');
+        }
+    });
 
-	$scope.goToCart = function($scope){
-		if($scope.cart){
-			$location.path('')
-		}
-	};
+    //console.log($resource);
+
+    $scope.$watch(authService.getAuthenticated, function(auth) {
+        $scope.authenticated = auth;
+    });
+
+    $scope.$watch(authService.getIsAdmin, function(admin) {
+        $scope.admin = admin;
+    });
+
+    $scope.sub = function() {
+
+
+    };
+
+
+
 });
 
-app.factory('msgService', function () {
-	var successMsg='';
-	var errorMsg='';
-	return {
-			setSuccessMessage: function(m) {
-					successMsg=m;
-			},
-			getSuccessMessage: function() {
-					return successMsg;
-			},
-			setErrorMessage: function(m){
-				errorMsg=m;
-			},
-			getErrorMessage: function(){
-				return errorMsg;
-			},
-			reset: function(){
-				this.setSuccessMessage('');
-				this.setErrorMessage('');
-			}
-	};
+app.controller('productCtrl', function($scope, $rootScope, $resource, $location, authService) {
+
+    $scope.$watch(authService.getAuthenticated, function(auth) {
+        $scope.authenticated = auth;
+    });
+
+    $scope.$watch(authService.getIsAdmin, function(admin) {
+        $scope.admin = admin;
+    });
+    var User = $resource('/api/user');
+    User.get({}, function(user) {
+        if (user.name) {
+            $scope.authenticated = true;
+            authService.setAuthenticated(true);
+            if (user.admin) {
+                $scope.admin = true;
+                authService.setIsAdmin(true);
+            }
+            var Products = $resource('/api/products');
+            Products.query({}, function(products, err) {
+                $scope.products = products;
+            });
+        } else {
+            $location.path('/login');
+        }
+    });
+
+    $scope.goToCart = function($scope) {
+        if ($scope.cart) {
+            $location.path('')
+        }
+    };
+
+    $scope.updateProd = function(prod) {
+        $location.path('/update_products');
+        $rootScope.prod = prod;
+        console.log(prod.productName);
+    };
 });
 
-app.factory('authService',function(){
-	var authenticated = false, isAdmin = false;
-	return {
-		setAuthenticated: function(auth){
-			authenticated = auth;
-		},
-		getAuthenticated: function(){
-			return authenticated;
-		},
-		setIsAdmin: function(admin){
-			isAdmin = admin;
-		},
-		getIsAdmin: function(){
-			return isAdmin;
-		},
-		reset: function(){
-			this.setAuthenticated(false);
-			this.setIsAdmin(false);
-		}
-	}
+app.factory('msgService', function() {
+    var successMsg = '';
+    var errorMsg = '';
+    return {
+        setSuccessMessage: function(m) {
+            successMsg = m;
+        },
+        getSuccessMessage: function() {
+            return successMsg;
+        },
+        setErrorMessage: function(m) {
+            errorMsg = m;
+        },
+        getErrorMessage: function() {
+            return errorMsg;
+        },
+        reset: function() {
+            this.setSuccessMessage('');
+            this.setErrorMessage('');
+        }
+    };
+});
+
+app.factory('authService', function() {
+    var authenticated = false,
+        isAdmin = false;
+    return {
+        setAuthenticated: function(auth) {
+            authenticated = auth;
+        },
+        getAuthenticated: function() {
+            return authenticated;
+        },
+        setIsAdmin: function(admin) {
+            isAdmin = admin;
+        },
+        getIsAdmin: function() {
+            return isAdmin;
+        },
+        reset: function() {
+            this.setAuthenticated(false);
+            this.setIsAdmin(false);
+        }
+    }
 });
--- routes/api.js
+++ routes/api.js
@@ -9,43 +9,43 @@ mongoose.Promise = global.Promise;
 //for add product using multer
 var multer = require('multer');
 var multerConf = {
-  storage: multer.diskStorage({
-    destination: function(req,file,next){
-      next(null,'./public/images');
-    },
-    filename:function(req,file,next){
-      console.log(file);
-      const ext = file.mimetype.split('/')[1];
-      next(null,file.originalname.substring(0,file.originalname.lastIndexOf('.'))+'.'+ext);
-      //next(null,req.body.imgname+'.'+ext);
+    storage: multer.diskStorage({
+        destination: function(req, file, next) {
+            next(null, './public/images');
+        },
+        filename: function(req, file, next) {
+            console.log(file);
+            const ext = file.mimetype.split('/')[1];
+            next(null, file.originalname.substring(0, file.originalname.lastIndexOf('.')) + '.' + ext);
+            //next(null,req.body.imgname+'.'+ext);
+        }
+    }),
+    fileFilter: function(req, file, next) {
+        if (!file) {
+            next();
+        }
+        const image = file.mimetype.startsWith('image/');
+        if (image) {
+            next(null, true);
+        } else {
+            next({ message: "file type not supported" }, false);
+        }
     }
-  }),
-  fileFilter : function(req,file,next){
-      if(!file){
-          next();
-      }
-      const image = file.mimetype.startsWith('image/');
-      if(image){
-          next(null,true);
-      }else{
-          next({message:"file type not supported"},false);
-      }
-  }  
 };
 
 
 //Used for routes that must be authenticated.
-function isAuthenticated (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler 
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
+function isAuthenticated(req, res, next) {
+    // if user is authenticated in the session, call the next() to call the next request handler 
+    // Passport adds this method to request object. A middleware is allowed to add properties to
+    // request and response objects
 
-	if (req.isAuthenticated()){
-		return next();
-	}
+    if (req.isAuthenticated()) {
+        return next();
+    }
 
-	// if the user is not authenticated then redirect him to the login page
-	return res.redirect('/#login');
+    // if the user is not authenticated then redirect him to the login page
+    return res.redirect('/#login');
 };
 
 //Register the authentication middleware
@@ -53,106 +53,108 @@ function isAuthenticated (req, res, next) {
 
 
 router.route('/user')
-    .get(function(req,res){
-        if(req.user){
-            return res.json({'name':req.user.name,'admin':req.user.admin});
+    .get(function(req, res) {
+        if (req.user) {
+            return res.json({ 'name': req.user.name, 'admin': req.user.admin });
         }
         return res.json({});
     });
 
-	// router.post('/products',upload.single('photoFile'),(req,res,next) => {
-	// 	console.log(req.body);
-	// 	console.log(req.file);  
-	// 	// Everything went fine 
-	//   });
-	//   // if(req.user.admin){
-	// 	  // console.log(req.body);
-	// 	  // console.log(req.file);
-	//   // }
-	//   // else{
-	// 	  // return res.json({'state': false, 'message':'You are not authorized to perform this operation!'});
-  
+// router.post('/products',upload.single('photoFile'),(req,res,next) => {
+// 	console.log(req.body);
+// 	console.log(req.file);  
+// 	// Everything went fine 
+//   });
+//   // if(req.user.admin){
+// 	  // console.log(req.body);
+// 	  // console.log(req.file);
+//   // }
+//   // else{
+// 	  // return res.json({'state': false, 'message':'You are not authorized to perform this operation!'});
+
 
 router.route('/products')
-	.get(function(req,res){
-		Product.find(function(err,products){
-			if(err){
-				return res.send(500,err);
-			}
-			return res.json(products);
-		});	
+    .get(function(req, res) {
+        Product.find(function(err, products) {
+            if (err) {
+                return res.send(500, err);
+            }
+            return res.json(products);
+        });
     });
 
 //router.post('/',multer(multerConf).single('productImage'),productCrud.addData);
-router.post('/products',multer(multerConf).single('productImage'),function(req,res){
+router.post('/products', multer(multerConf).single('productImage'), function(req, res) {
     //res.send("hello in api.js");
-	var postBody = req.body;
-	console.log("post:"+req.file.originalname);
+    var postBody = req.body;
+    console.log("post:" + req.file.originalname);
     var data = {
         productName: postBody.productName,
         productCategory: postBody.productCategory,
         productPrice: postBody.productPrice,
         productImage: req.file.originalname,
         stockQuantity: postBody.stockQuantity
-	}
+    }
     console.log(data);
 
     var saveData = new Product(data);
 
-    saveData.save(function(err,data){
-        if(err){
+    saveData.save(function(err, data) {
+        if (err) {
             res.send(err);
-        }else{
-			res.redirect('/#!manage_products');
-		}
-	});
-	
+        } else {
+            res.redirect('/#!manage_products');
+        }
+    });
+
 });
 
 
-router.put('/products/:id',multer(multerConf).single('productImage'), function(req,res){
+router.put('/products/:id', multer(multerConf).single('productImage'), function(req, res) {
     var putBody = req.body;
-    
-    console.log("put:"+req.file);
-    Product.findById(req.params.id,function(err,product){
-        if(err){
+
+    console.log("put:" + req.file);
+    Product.findById(req.params.id, function(err, product) {
+        if (err) {
             res.send(err);
         }
-        product.productName =  putBody.productName;
-        product.productCategory =  putBody.productCategory;
+        product.productName = putBody.productName;
+        product.productCategory = putBody.productCategory;
         product.productPrice = putBody.productPrice;
         product.stockQuantity = putBody.stockQuantity;
-        req.file?product.productImage = req.file.originalname:{};
-        putBody.isDeleted?product.isDeleted = putBody.isDeleted:{};
-    
+        req.file ? product.productImage = req.file.originalname : {};
+        putBody.isDeleted ? product.isDeleted = putBody.isDeleted : {};
+
         console.log(product);
-        product.save(function(err){
-            if(err){
+        product.save(function(err) {
+            if (err) {
                 res.send(err);
             }
-            res.json({state: 'Updated Successfully!'});
+            res.json({ state: 'Updated Successfully!' });
         });
     });
 });
 
-router.delete('/products/:id',function(req,res){
-    Product.findById(req.params.id,function(err,product){
-        if(err){
+router.delete('/products/:id', function(req, res) {
+    Product.findById(req.params.id, function(err, product) {
+        if (err) {
             res.send(err);
         }
         product.isDeleted = true;
-        product.save(function(err){
-            if(err){
+        product.save(function(err) {
+            if (err) {
                 res.send(err);
             }
-            res.json({state: 'Safe delete complete'});
+            res.json({ state: 'Safe delete complete' });
         });
-        
+
     });
 });
-	// }
+
+
+// }
 // router.route('/posts')
-    
+
 //     // return all posts
 //     .get(function(req,res){
 //         Post.find(function(err,posts){
